332fac07b4e0d766023750a909e06d5d83651a4e
gauthamramu121 <gauthamramu121@gmail.com>
2026-06-10 10:21:15 +0530

    deleted duplicate backend folder

--- Inventory Angular/nextcart/src/main/java/com/example/products/exceptions/customExceptions/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.products.exceptions.customExceptions;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
