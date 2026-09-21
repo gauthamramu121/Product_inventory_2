1f5b5ef338a5a123d7158541ba28d40d32adef96
gauthamramu121 <gauthamramu121@gmail.com>
2026-09-21 14:35:40 +0530

    added ip hosting for testing

--- Inventory Angular/nextcart/src/app/model/constant.ts
+++ Inventory Angular/nextcart/src/app/model/constant.ts
@@ -1,5 +1,6 @@
 // export const AUTH_URL = 'http://localhost:8183/auth';
 export const BASE_URL = 'http://localhost:8332/product';
+// export const BASE_URL = 'http://192.168.0.170:8332/product';
 
 export const IMAGE_URL = 'http://localhost:8332/uploads/';
 
