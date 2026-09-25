acc2ada38cd591a4a1c7a5c069b93523fc041caa
gauthamramu121 <gauthamramu121@gmail.com>
2026-09-25 15:46:16 +0530

    changed the local url base to deployed url

--- Inventory Angular/nextcart/src/app/model/constant.ts
+++ Inventory Angular/nextcart/src/app/model/constant.ts
@@ -1,5 +1,6 @@
 // export const AUTH_URL = 'http://localhost:8183/auth';
-export const BASE_URL = 'http://localhost:8332/product';
+// export const BASE_URL = 'http://localhost:8332/product';
+export const BASE_URL = 'https://product-inventory-2-git-gauthamramu121-dev.apps.rm1.0a51.p1.openshiftapps.com/product';
 // export const BASE_URL = 'http://192.168.0.170:8332/product';
 
 export const IMAGE_URL = 'http://localhost:8332/uploads/';
